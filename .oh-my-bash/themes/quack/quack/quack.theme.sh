3bac57f3081cd20ded68c77055841ad0f0307b58
s0586688 <66282159+jusnim@users.noreply.github.com>
2024-05-26 07:01:30 +0200

    init

---
+++ .oh-my-bash/themes/quack/quack/quack.theme.sh
@@ -0,0 +1,80 @@
+#! bash oh-my-bash.module
+
+# BASED ON PURITY AND BINARYANOMALY THEME 
+SCM_THEME_PROMPT_DIRTY=" ${_omb_prompt_bold_brown}⊘${_omb_prompt_normal}"
+SCM_THEME_PROMPT_CLEAN=" ${_omb_prompt_bold_green}✓${_omb_prompt_normal}"
+SCM_THEME_PROMPT_PREFIX="${_omb_prompt_reset_color}( "
+SCM_THEME_PROMPT_SUFFIX=" ${_omb_prompt_reset_color})"
+
+GIT_THEME_PROMPT_DIRTY=" ${_omb_prompt_bold_brown}⊘${_omb_prompt_normal}"
+GIT_THEME_PROMPT_CLEAN=" ${_omb_prompt_bold_green}✓${_omb_prompt_normal}"
+GIT_THEME_PROMPT_PREFIX="${_omb_prompt_reset_color}( "
+GIT_THEME_PROMPT_SUFFIX=" ${_omb_prompt_reset_color})"
+
+OMB_THEME_PURITY_STATUS_BAD="${_omb_prompt_brown}❯${_omb_prompt_reset_color}${_omb_prompt_normal} "
+OMB_THEME_PURITY_STATUS_OK="${_omb_prompt_green}❯${_omb_prompt_reset_color}${_omb_prompt_normal} "
+_omb_deprecate_declare 20000 STATUS_THEME_PROMPT_BAD OMB_THEME_PURITY_STATUS_BAD sync
+_omb_deprecate_declare 20000 STATUS_THEME_PROMPT_OK  OMB_THEME_PURITY_STATUS_OK  sync
+
+
+# Set different host color for local and remote sessions
+function set_host_color() {
+  # Detect if connection is through SSH
+  if [[ ! -z $SSH_CLIENT ]]; then
+    printf "${lime_yellow}"
+  else
+    printf "${light_orange}"
+  fi
+}
+
+
+# Set different username color for users and root
+function set_user_color() {
+  case $(id -u) in
+    0)
+      printf "${_omb_prompt_brown}"
+      ;;
+    *)
+      printf "${_omb_prompt_teal}"
+      ;;
+  esac
+}
+
+function set_custom_colors() {
+dark_grey="\[$(tput setaf 8)\]"
+dark_purple="\[$(tput setaf 104)\]"
+}
+# Initize custom colors
+
+function __ps_time {
+	echo "$(clock_prompt)${_omb_prompt_normal}"
+}
+function _omb_theme_PROMPT_COMMAND {
+  if (($? == 0)); then
+    local ret_status=${STATUS_THEME_PROMPT_OK:-${OMB_THEME_PURITY_STATUS_OK-}}
+    local status_code=""
+  else
+    local ret_status=${STATUS_THEME_PROMPT_BAD:-${OMB_THEME_PURITY_STATUS_BAD-}}
+    local status_code="${_omb_prompt_brown}[\$?]"
+  fi
+  ps_username="$(set_user_color)\u${_omb_prompt_normal}"
+  ps_uh_separator="${dark_purple}@${_omb_prompt_normal}"
+
+  local hostn="\h"
+  if [[ "$HOSTNAME" == "quack" ]]; then hostn="🦆"; fi
+	
+  rightprompt() {
+     printf "\n%*s"  $COLUMNS "${status_code}"
+  }
+
+  
+  ps_hostname="$(set_host_color)${hostn}${_omb_prompt_normal}"
+ 
+
+ PS1="\[$(tput sc; rightprompt; tput rc)\]\n$(__ps_time) ${_omb_prompt_navy}\w $(scm_prompt_info)\n${ps_username}${ps_uh_separator}${ps_hostname} ${ret_status}"
+}
+
+
+set_custom_colors
+THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$dark_grey"}
+_omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
