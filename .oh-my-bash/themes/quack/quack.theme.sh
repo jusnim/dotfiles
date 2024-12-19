3bdc732fc7ac9a51832bdd44c67555fc35dcade0
Jusnim <66282159+jusnim@users.noreply.github.com>
2024-12-19 20:32:11 +0100

    fixed display of venv

--- .oh-my-bash/themes/quack/quack.theme.sh
+++ .oh-my-bash/themes/quack/quack.theme.sh
@@ -1,5 +1,10 @@
 #! bash oh-my-bash.module
 
+function set_custom_colors() {
+   dark_grey="\[$(tput setaf 8)\]"
+   dark_purple="\[$(tput setaf 104)\]"
+}
+
 # BASED ON PURITY AND BINARYANOMALY THEME 
 SCM_THEME_PROMPT_DIRTY=" ${_omb_prompt_bold_brown}⊘${_omb_prompt_normal}"
 SCM_THEME_PROMPT_CLEAN=" ${_omb_prompt_bold_green}✓${_omb_prompt_normal}"
@@ -10,6 +15,9 @@ GIT_THEME_PROMPT_DIRTY=" ${_omb_prompt_bold_brown}⊘${_omb_prompt_normal}"
 GIT_THEME_PROMPT_CLEAN=" ${_omb_prompt_bold_green}✓${_omb_prompt_normal}"
 GIT_THEME_PROMPT_PREFIX="${_omb_prompt_reset_color}( "
 GIT_THEME_PROMPT_SUFFIX=" ${_omb_prompt_reset_color})"
+OMB_PROMPT_VIRTUALENV_FORMAT="%s »"
+OMB_PROMPT_SHOW_PYTHON_VENV=${OMB_PROMPT_SHOW_PYTHON_VENV:=true}
+
 
 OMB_THEME_PURITY_STATUS_BAD="${_omb_prompt_brown}❯${_omb_prompt_reset_color}${_omb_prompt_normal} "
 OMB_THEME_PURITY_STATUS_OK="${_omb_prompt_green}❯${_omb_prompt_reset_color}${_omb_prompt_normal} "
@@ -40,10 +48,6 @@ function set_user_color() {
   esac
 }
 
-function set_custom_colors() {
-   dark_grey="\[$(tput setaf 8)\]"
-   dark_purple="\[$(tput setaf 104)\]"
-}
 
 function __ps_time {
    echo "$(clock_prompt)${_omb_prompt_normal}"
@@ -59,17 +63,21 @@ function _omb_theme_PROMPT_COMMAND {
   fi
   ps_username="$(set_user_color)\u${_omb_prompt_normal}"
   ps_uh_separator="${dark_purple}@${_omb_prompt_normal}"
+  ps_virtualenv="${dark_purple}$(_omb_prompt_print_python_venv)${_omb_prompt_normal} "
 
   local hostn="\h"
   if [[ "$HOSTNAME" == "quack" ]]; then hostn="🦆"; fi
+  if [[ "$HOSTNAME" == "Superball" ]]; then hostn="🔮"; fi
+  if [[ "$HOSTNAME" == "frog" ]]; then hostn="🐸"; fi
 	
   rightprompt() {
      printf "\n%*s"  $COLUMNS "${status_code}"
   }
 
  ps_hostname="$(set_host_color)${hostn}${_omb_prompt_normal}"
- PS1="\[$(tput sc; rightprompt; tput rc)\]\n$(__ps_time) ${_omb_prompt_navy}\w $(scm_prompt_info)\n${ps_username}${ps_uh_separator}${ps_hostname} ${ret_status}"
+ PS1="\[$(tput sc; rightprompt; tput rc)\]\n$(__ps_time) ${_omb_prompt_navy}\w $(scm_prompt_info)\n${ps_virtualenv}${ps_username}${ps_uh_separator}${ps_hostname} ${ret_status}"
 }
+
 
 # initialize custom colors
 set_custom_colors
